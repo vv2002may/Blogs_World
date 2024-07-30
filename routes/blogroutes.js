d5384ea97a5f89ce301b4c12ed4ad852852e7192
Vimal <home@Vimals-MacBook-Air.local>
2024-07-31 01:19:48 +0530

    .

--- routes/blogroutes.js
+++ routes/blogroutes.js
@@ -5,72 +5,14 @@ const User = require('../models/users');
 
 var userName = '';
 var userEmail = '';
+var blogs = '';
 router.get('/about', (req, res) => {
    // res.send('<p>about!</p>');
    // res.sendFile('./views/about.html', {root:__dirname});
 
-   res.render('about', { currentPage:'about',  title: 'About', userName });
+   res.render('about', { currentPage: 'about', title: 'About', userName });
+   next();
 })
-
-router.get('/signout', (req, res) => {
-   userName = '';
-   userEmail = '';
-   res.redirect('/');
-});
-router.get('/', (req, res) => {
-   // res.send('<p>home!</p>');
-   // res.sendFile('./views/index.html', {root:__dirname});
-
-   Blog.find().sort({ createdAt: -1 })
-      .then((result) => {
-         res.render('index', { currentPage:'/', title: 'All Blogs', userName, blogs: result });
-      })
-})
-router.get('/userhome', (req, res) => {
-   Blog.find({ email: userEmail }).sort({ createdAt: -1 })
-      .then((result) => {
-         res.render('userhome', { currentPage:'userhome',  title: userName, userName, blogs: result });
-      })
-});
-router.get('/signin', (req, res) => {
-   res.render('signin', { currentPage:'signin',  title: 'Sign in', userName, message: '', newaccount: '' });
-});
-
-router.post('/signin', (req, res) => {
-   const { email, password } = req.body;
-
-   User.findOne({ email })
-      .then((result) => result)
-      .then((result) => {
-         if (result.password === password) {
-            userName = result.name;
-            userEmail = result.email;
-            return res.redirect('/blogs');
-         }
-         else {
-            return res.render('signin', { currentPage:'signin',  title: 'Sign in', userName, message: 'Password is incorrect!', newaccount: '' });
-         }
-      })
-      .catch((err) => {
-         return res.render('signin', { currentPage:'signin',  title: 'Sign in', userName, message: 'User not exist!', newaccount: 'create a new account' });
-      });
-});
-
-router.get('/signup', (req, res) => {
-   res.render('signup', { currentPage:'signup',  title: 'Sign up', userName, message: '', newaccount: '' });
-});
-
-router.post('/signup', (req, res) => {
-   const user = new User(req.body);
-   user.save()
-      .then((result) => {
-         res.render('signup', { currentPage:'signup',  title: 'Sign up', userName, message: 'Account Created!', newaccount: 'Log in' });
-      })
-      .catch((err) => {
-         console.log(err);
-      });
-});
-
 router.post('/', (req, res) => {
    // console.log(req.body);
    const data = {
@@ -89,32 +31,118 @@ router.post('/', (req, res) => {
       });
 })
 router.get('/create', (req, res) => {
-   res.render('create', { currentPage:'create',  title: 'Create a new blog', userName });
+   res.render('create', { currentPage: 'create', title: 'Create a new blog', userName });
 })
+
+router.get('/signin', (req, res) => {
+   res.render('signin', { currentPage: 'signin', title: 'Sign in', userName, message: '', newaccount: '' });
+});
+
+router.post('/signin', (req, res) => {
+   const { email, password } = req.body;
+
+   User.findOne({ email })
+      .then((result) => result)
+      .then((result) => {
+         if (result.password === password) {
+            userName = result.name;
+            userEmail = result.email;
+            return res.redirect('/blogs');
+         }
+         else {
+            return res.render('signin', { currentPage: 'signin', title: 'Sign in', userName, message: 'Password is incorrect!', newaccount: '' });
+         }
+      })
+      .catch((err) => {
+         return res.render('signin', { currentPage: 'signin', title: 'Sign in', userName, message: 'User not exist!', newaccount: 'create a new account' });
+      });
+});
+
+router.get('/signup', (req, res) => {
+   res.render('signup', { currentPage: 'signup', title: 'Sign up', userName, message: '', newaccount: '' });
+});
+
+router.post('/signup', (req, res) => {
+   const user = new User(req.body);
+   // console.log(`${user.email}`);
+   const email = req.body.email;
+   User.findOne({ email })
+      .then((result) => result)
+      .then((result) => {
+         res.render('signup', { currentPage: 'signup', title: 'Sign up', userName, message: 'User is already registered!', newaccount: 'Log in' });
+      })
+      .catch((err) => {
+         user.save()
+            .then((result) => {
+               res.render('signup', { currentPage: 'signup', title: 'Sign up', userName, message: 'Account Created!', newaccount: 'Log in' });
+            })
+            .catch((err) => {
+               console.log(err);
+            });
+      });
+
+   // user.save()
+   //    .then((result) => {
+   //       res.render('signup', { currentPage: 'signup', title: 'Sign up', userName, message: 'Account Created!', newaccount: 'Log in' });
+   //    })
+   //    .catch((err) => {
+   //       console.log(err);
+   //    });
+});
+router.get('/signout', (req, res) => {
+   userName = '';
+   userEmail = '';
+   res.redirect('/');
+});
+router.get('/', (req, res) => {
+   // res.send('<p>home!</p>');
+   // res.sendFile('./views/index.html', {root:__dirname});
+
+   Blog.find().sort({ createdAt: -1 })
+      .then((result) => {
+         res.render('index', {blog_id:'', currentPage: '/', details:'no', message: '', title: 'All Blogs', blog: '', userName, blogs: result });
+      })
+})
+
 router.get('/:id', (req, res) => {
    const id = req.params.id;
    // console.log(id);
+   Blog.find().sort({ createdAt: -1 })
+      .then((result) => {
+         blogs = result;
+      })
    Blog.findById(id)
       .then((result) => {
-         res.render('details', { currentPage:'details',  message:'', blog: result, title: 'Blog Details', userName });
+         res.render('index', {blog_id:id, currentPage: 'details', details:'yes', message: '', blog: result,blogs, title: 'Blog Details', userName });
       })
       .catch((err) => {
          console.log(err);
       });
 })
 
+router.get('/userhome', (req, res) => {
+   Blog.find({ email: userEmail }).sort({ createdAt: -1 })
+      .then((result) => {
+         res.render('userhome', { currentPage: 'userhome', title: userName, userName, blogs: result });
+      })
+});
+
+
+
+
+
 
 router.get('/delete/:id', (req, res) => {
    const id = req.params.id;
    Blog.deleteOne({ _id: id, email: userEmail })
       .then((result) => {
-         if(result.deletedCount > 0)
+         if (result.deletedCount > 0)
             res.redirect('/blogs');
          else {
             Blog.findById(id)
-            .then((result) => {
-               res.render('details', { currentPage:'details',  message:'You do not have permission to delete this blog!', blog: result, title: 'Blog Details', userName });
-            })
+               .then((result) => {
+                  res.render('details', { currentPage: 'details', message: 'You do not have permission to delete this blog!', blog: result, title: 'Blog Details', userName });
+               })
          }
       })
       .catch((err) => {
